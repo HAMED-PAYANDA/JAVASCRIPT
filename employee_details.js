1a052c23381686976d1500d415803c4ca9e405ff
HAMED-PAYANDA <hamedpayanda@gmail.com>
2025-08-28 05:40:39 -0400

    Update employee_details.js

--- employee_details.js
+++ employee_details.js
@@ -24,3 +24,13 @@ function displayHREmployees() {
       document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
 }
 
+// Function to find an employee by ID
+function findEmployeeById(employeeId) {
+      const foundEmployee = employees.find(employee => employee.id === employeeId);
+      if (foundEmployee) {
+      document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+      }
+      else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+       }
+   }
